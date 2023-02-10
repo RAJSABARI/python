6c95629fff68d5bfe1983ef0a18f497d72e8c786
yourstore2002 <yourstorestartup@gmail.com>
2023-02-10 20:12:36 +0530

    python

---
+++ primenumber.py
@@ -0,0 +1,7 @@
+user = int(input("enter a number"))
+if(2 == user):
+    print("yes it is prime number")
+elif(user%2 != 0):
+    print("yes it is prime number")
+else:
+    print("its is not a prime number")
