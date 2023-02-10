0eb3cd272c5500e6ac1741c0d48da11cb7cd7b3c
yourstore2002 <yourstorestartup@gmail.com>
2023-02-10 20:25:26 +0530

    python

---
+++ odd or even.py
@@ -0,0 +1,8 @@
+user = int(input("enter a number  "))
+if(user == 0 | 1):
+    print("it is a relational number")
+else:
+    if(user%2 == 0):
+        print("it is a even number")
+    else:
+        print("it is a odd number")
